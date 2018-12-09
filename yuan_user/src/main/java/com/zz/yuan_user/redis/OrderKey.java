7a2fb30380ffa93dd262c18a0d625e0dad499da8
GXwar <richardxulh@gmail.com>
2018-12-09 00:18:57 -0500

    bug fixes

--- yuan_user/src/main/java/com/zz/yuan_user/redis/OrderKey.java
+++
@@ -1,8 +0,0 @@
-package com.zz.yuan_user.redis;
-
-public class OrderKey extends BasePrefix {
-
-    private OrderKey(int expireSecond, String prefix) {
-        super(expireSecond, prefix);
-    }
-}
--- yuan_user/src/main/java/com/zz/yuan_user/util/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package com.zz.yuan_user.util;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-    public static String uuid(){
-        return UUID.randomUUID().toString().replace("-","");
-    }
-}
